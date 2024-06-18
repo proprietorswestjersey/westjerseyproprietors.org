9b526d7c1442babd7e73573e0a5d53386d2d4191
Phips Peter <phips@asana.com>
2024-06-18 14:06:50 -0400

    Add styles
    Add a basic build system and styles.

--- src/index.md
+++ src/index.md
@@ -1,1 +1,6 @@
+---
+title: West Jersey Proprietors
+layout: base.11ty.js
+---
+
 # West Jersey Proprietors
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
